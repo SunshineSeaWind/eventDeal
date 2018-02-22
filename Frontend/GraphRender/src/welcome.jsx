60d9e7ddd568ed02c33be3e4cb80d01eefede2de
boy56 <duanjian56@126.com>
2018-02-22 11:59:22 +0800

    ==

--- Frontend/GraphRender/src/welcome.jsx
+++ Frontend/GraphRender/src/welcome.jsx
@@ -11,11 +11,11 @@ export default class WelcomePage extends React.Component {
           <Destination description='OfficialRelations'
                       href='#/galaxy/Relations?cx=-1&cy=0&cz=177&lx=0.0000&ly=0.0000&lz=0.0000&lw=1.0000&ml=150&s=1.75&l=1'
                       media='brew_fly_first.png'
-                      name='Relations'/>
+                      name='OfficialRelations'/>
           <Destination description='OfficialTimeline'
                       href='#/galaxy/NewTimeline?l=1'
                       media='composer_fly_first.png'
-                      name='NewTimeline'/>
+                      name='OfficialTimeline'/>
         </div>
       </div>
     );
