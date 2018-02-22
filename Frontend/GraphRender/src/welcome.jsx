cd97272324403e9927ca2251cc565b09f6324c45
boy56 <duanjian56@126.com>
2018-02-22 11:57:28 +0800

    change of graph name

--- Frontend/GraphRender/src/welcome.jsx
+++ Frontend/GraphRender/src/welcome.jsx
@@ -8,11 +8,11 @@ export default class WelcomePage extends React.Component {
         <h1>Welcome to the Code Galaxies, Commander</h1>
         <h2>Choose your destination:</h2>
         <div className='media-list'>
-          <Destination description='Relations'
+          <Destination description='OfficialRelations'
                       href='#/galaxy/Relations?cx=-1&cy=0&cz=177&lx=0.0000&ly=0.0000&lz=0.0000&lw=1.0000&ml=150&s=1.75&l=1'
                       media='brew_fly_first.png'
                       name='Relations'/>
-          <Destination description='NewTimeline'
+          <Destination description='OfficialTimeline'
                       href='#/galaxy/NewTimeline?l=1'
                       media='composer_fly_first.png'
                       name='NewTimeline'/>
