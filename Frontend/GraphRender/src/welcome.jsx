ac976b5f5d155b634f080b5a22e62bb2f791603a
boy56 <duanjian56@126.com>
2018-03-12 20:07:01 +0800

    the change of graph's view

--- Frontend/GraphRender/src/welcome.jsx
+++ Frontend/GraphRender/src/welcome.jsx
@@ -12,7 +12,7 @@ export default class WelcomePage extends React.Component {
                       href='#/galaxy/OfficialRelations?cx=-1&cy=0&cz=177&lx=0.0000&ly=0.0000&lz=0.0000&lw=1.0000&ml=150&s=1.75&l=1'
                       media='brew_fly_first.png'
                       name='OfficialRelations'/>
-					  {/*<Destination description='OfficialTimeline'
+		  {/*<Destination description='OfficialTimeline'
                       href='#/galaxy/OfficialTimeline?l=1'
                       media='composer_fly_first.png'
                       name='OfficialTimeline'/>
@@ -22,7 +22,7 @@ export default class WelcomePage extends React.Component {
                       name='OfficialTimelineAddData'/>
 					  */}
           <Destination description='OfficialTimelineTimeNodeConnect'
-                      href='#/galaxy/OfficialTimelineTimeNodeConnect?l=1'
+                      href='#/galaxy/OfficialTimelineTimeNodeConnect?cx=-7581&cy=-615&cz=6765&lx=0.0332&ly=-0.4439&lz=0.0229&lw=0.8952&ml=150&s=1.75&l=1&v=2018-3-7T00-00-00Z'
                       media='composer_fly_first.png'
                       name='OfficialTimeline'/>
           <Destination description='TestGraphTest'
@@ -30,11 +30,11 @@ export default class WelcomePage extends React.Component {
                       media='composer_fly_first.png'
                       name='TestGraphTest'/>
           <Destination description='UnofficialRelations'
-                          href='#/galaxy/UnofficialRelations?l=1'
+                          href='#/galaxy/UnofficialRelations?cx=-4114&cy=-4044&cz=1837&lx=0.3022&ly=-0.8714&lz=0.3848&lw=0.0360&ml=150&s=1.75&l=1&v=2018-2-20T00-00-00Z'
                           media='composer_fly_first.png'
                           name='UnofficialRelations'/>
           <Destination description='UnofficialTimeline'
-                          href='#/galaxy/UnofficialTimeline?l=1'
+                          href='#/galaxy/UnofficialTimeline?cx=350&cy=653&cz=-15875&lx=0.1818&ly=-0.9793&lz=0.0712&lw=0.0532&ml=150&s=1.75&l=1&v=2018-2-20T00-00-00Z'
                           media='composer_fly_first.png'
                           name='UnofficialTimeline'/>
 			
